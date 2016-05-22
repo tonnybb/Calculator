928efc68cb8c347297369949e2c5ffe2aca94812
Tonny Boesenbaek <tonnyb@gmail.com>
2016-05-21 20:24:48 -0400

    Add exception class: MultipleConsecutiveOperatorsDetectedException

---
+++ Calculator/MultipleConsecutiveOperatorsDetectedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class MultipleConsecutiveOperatorsDetectedException: Exception
+    {
+        public MultipleConsecutiveOperatorsDetectedException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
